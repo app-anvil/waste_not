5b76a7b4f13c772c94715779c565845029ac31c8
Marco Trivisonno <marco.trivi2@gmail.com>
2024-03-19 23:16:55 +0100

    fix: add openedAt param to add item feature
    Closes #36

--- packages/items_repository/lib/src/data/data_sources/items_provider.dart
+++ packages/items_repository/lib/src/data/data_sources/items_provider.dart
@@ -4,11 +4,17 @@ import 'package:storages_repository/storages_repository.dart';
 import '../../../items_repository.dart';
 
 abstract interface class ItemsProvider {
+  /// Most of the time an item is added with openedAt equal to null.
+  ///
+  /// Use [openedAt] when an opened item is deleted from the db and the
+  /// user wants to undo the operation. So a new item is created and [openedAt]
+  /// field should be set.
   Future<ItemModel> add({
     required ProductEntity product,
     required DateTime expirationDate,
     required Measure remainingMeasure,
     required StorageEntity storage,
+    required DateTime? openedAt,
     int? shelf,
   });
 
